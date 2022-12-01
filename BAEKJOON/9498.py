b6a1b2a8a931d17afb7f225304a015b802f189f2
CCI1997 <100206453+CCI1997@users.noreply.github.com>
2022-12-01 23:13:55 +0900

    9498.py
    조건문
    2단계
    9498번
    시험 성적

---
+++ BAEKJOON/9498.py
@@ -0,0 +1,14 @@
+# 입력
+score = int(input())
+
+# 출력
+if score >= 90 and score <= 100:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+elif score >= 60:
+    print("D")
+else:
+    print("F")
