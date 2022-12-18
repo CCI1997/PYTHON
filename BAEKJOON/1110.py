09f4e7f10f4d28938699b58198acf6c06398defd
CCI1997 <100206453+CCI1997@users.noreply.github.com>
2022-12-18 22:56:38 +0900

    1110.py
    반복문
    12단계
    1110번
    더하기 사이클

---
+++ BAEKJOON/1110.py
@@ -0,0 +1,17 @@
+# 입력
+N = int(input())
+
+num = N
+count = 0         
+
+while True:
+    a = num // 10
+    b = num % 10
+    c = (a + b) % 10
+    num = (b * 10) + c
+    count += 1
+    if(num == N):
+        break
+ 
+# 출력
+print(count)
