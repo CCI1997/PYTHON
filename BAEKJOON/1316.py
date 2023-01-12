171dde5eabdf93107800c0e416fadb8d610e40f9
CCI1997 <100206453+CCI1997@users.noreply.github.com>
2023-01-12 21:55:40 +0900

    1316.py
    문자열
    10단계
    1316번
    그룹 단어 체커

---
+++ BAEKJOON/1316.py
@@ -0,0 +1,16 @@
+# 입력
+N = int(input())
+
+cnt = N
+
+for i in range(N):
+    word = input()
+    for j in range(0, len(word) - 1):
+        if word[j] == word[j + 1]:
+            pass
+        elif word[j] in word[j + 1 :]:
+            cnt -= 1
+            break
+
+# 출력
+print(cnt)
